69d7de3060e4b14bacbebdc52e6abb999a797a17
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-06-23 09:07:34 +0700

    fix bugs

--- index.js
+++ index.js
@@ -2,50 +2,33 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import { Database } from "./src/Database/index.js";
 import chalk from "chalk";
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const port = 5000;
-export const prisma = new PrismaClient();
 
-async function main() {
-  app.use(express.json());
-  app.use(cors({ origin: "*" }));
-  app.use(morgan("dev"));
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  app.get("/", (req, res) => {
-    res.json({
-      message: "Welcome! to my server👋🏻",
-    });
+// Connect To Database
+Database(process.env.DATABASE_URL);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome! to my server👋🏻",
   });
+});
 
-  app.listen(port, () =>
-    console.log(
-      `${chalk.bgGreen(" INFO ")} Server running on ${chalk.blue.bold(
-        `http://localhost:${port}`
-      )}`
-    )
-  );
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-    console.log(
-      `${chalk.bgGreen(" INFO ")} ${chalk.blue(
-        "MongoDB connected successfully"
-      )}`
-    );
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    console.log(
-      `${chalk.bgRed(" ERROR ")} MongoDB not connected : ${chalk.red(error)}`
-    );
-    process.exit(1);
-  });
+// PORT
+const port = 3000;
+app.listen(port, () =>
+  console.log(
+    `${chalk.bgGreen(" INFO ")} Server running on ${chalk.blue.bold(
+      `http://localhost:${port}`
+    )}`
+  )
+);
